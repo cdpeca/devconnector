8577abaec85f4402fd5e65f9b54e75f4d42e6156
cdpeca <chrispeca+github@hotmail.com>
2021-01-15 17:45:43 -0600

    added required name check to users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+    '/',
+    [check('name', 'Name is required').not().isEmpty()],
+    (req, res) => {
+        console.log(req.body);
+        res.send('User Route');
+    },
+);
 
 module.exports = router;
