d1372f043ec237cdfbf23e8819cb4cc0e6c454c1
cdpeca <chrispeca+github@hotmail.com>
2021-01-18 13:12:55 -0600

    moved db module out of config and into root

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
-const connectDB = require('./config/db');
+const connectDB = require('./db');
 
 const app = express();
 
