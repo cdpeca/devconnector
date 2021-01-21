23f39e9d2fd0d39bc3b27ad62aa106f4e0e143dd
cdpeca <chrispeca+github@hotmail.com>
2021-01-20 21:50:37 -0600

    created profile action with intial function getCurrentProfile

---
+++ client/src/actions/profile.js
@@ -0,0 +1,24 @@
+import api from '../utils/api';
+import { setAlert } from './alert';
+
+import { GET_PROFILE, PROFILE_ERROR } from './types';
+
+// Get current user's profile
+export const getCurrentProfile = () => async dispatch => {
+    try {
+        const res = await api.get('/profile/me');
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
