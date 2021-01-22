8eb2cf76c408f4d9cbe0a1ee74368d5889f335c5
cdpeca <chrispeca+github@hotmail.com>
2021-01-22 16:53:55 -0600

    updated profile reducer to return null profile on PROFILE_ERROR

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -23,6 +23,7 @@ function profileReducer(state = initialState, action) {
                 ...state,
                 error: payload,
                 loading: false,
+                profile: null,
             };
         case CLEAR_PROFILE:
             return {
