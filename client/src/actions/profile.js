013faed0079538c140cddef62bac8bd8a7b0125e
cdpeca <chrispeca+github@hotmail.com>
2021-01-23 00:12:13 -0600

    created delete deleteExperience and deleteEducation fucntions to profile actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,13 @@
 import api from '../utils/api';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
+import {
+    CLEAR_PROFILE,
+    GET_PROFILE,
+    PROFILE_ERROR,
+    UPDATE_PROFILE,
+    ACCOUNT_DELETED,
+} from './types';
 
 // Get current user's profile
 export const getCurrentProfile = () => async dispatch => {
@@ -118,5 +124,71 @@ export const addEducation = (formData, history) => async dispatch => {
                 status: err.response.status,
             },
         });
+    }
+};
+
+// Delete experience
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await api.delete(`/profile/experience/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data,
+        });
+
+        dispatch(setAlert('Experience Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+// Delete education
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await api.delete(`/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data,
+        });
+
+        dispatch(setAlert('Education Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+// Delete account and profile
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+        try {
+            const res = await api.delete('/profile');
+
+            dispatch({ type: CLEAR_PROFILE });
+            dispatch({ type: ACCOUNT_DELETED });
+
+            dispatch(setAlert('Your account has been permanently deleted'));
+        } catch (err) {
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: {
+                    msg: err.response.statusText,
+                    status: err.response.status,
+                },
+            });
+        }
     }
 };
