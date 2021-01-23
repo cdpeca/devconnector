7bfebca661fa899e40342daab65b0010696985bb
cdpeca <chrispeca+github@hotmail.com>
2021-01-22 18:36:22 -0600

    added EditProfile component to App as a private route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -50,6 +51,11 @@ const App = () => {
                                 path='/create-profile'
                                 component={CreateProfile}
                             />
+                            <PrivateRoute
+                                exact
+                                path='/edit-profile'
+                                component={EditProfile}
+                            />{' '}
                         </Switch>
                     </section>
                 </Fragment>
