d6faff777d6ad4b4df8eacc83713b2d7acd2dece
cdpeca <chrispeca+github@hotmail.com>
2021-01-23 02:29:24 -0600

    defect(profile action): type declaration was missing on on getProfiles for CLEAR_PROFILE

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -33,7 +33,7 @@ export const getCurrentProfile = () => async dispatch => {
 
 // Get all profiles
 export const getProfiles = () => async dispatch => {
-    dispatch({ CLEAR_PROFILE });
+    dispatch({ type: CLEAR_PROFILE });
 
     try {
         const res = await api.get('/profile');
