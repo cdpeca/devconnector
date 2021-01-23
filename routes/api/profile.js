3e1b5abf93a8289ea50873cb55dfa01f0cb6c7c6
cdpeca <chrispeca+github@hotmail.com>
2021-01-22 18:31:04 -0600

    updated profile route api as skills was not being interpreted as an array during an edit profile action post

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -74,7 +74,9 @@ router.post(
         if (status) profileFields.status = status;
         if (githubusername) profileFields.githubusername = githubusername;
         if (skills) {
-            profileFields.skills = skills.split(',').map(skill => skill.trim());
+            profileFields.skills = Array.isArray(skills)
+                ? skills
+                : skills.split(',').map(skill => ' ' + skill.trim());
         }
 
         // Build social object
