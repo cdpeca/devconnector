d799560df18638cd1e757a505049da1ec8788e26
cdpeca <chrispeca+github@hotmail.com>
2021-01-23 02:34:11 -0600

    added Profiles component to App

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -43,6 +44,11 @@ const App = () => {
                                 component={Register}
                             />
                             <Route exact path='/login' component={Login} />
+                            <Route
+                                exact
+                                path='/profiles'
+                                component={Profiles}
+                            />
                             <PrivateRoute
                                 exact
                                 path='/dashboard'
@@ -57,17 +63,17 @@ const App = () => {
                                 exact
                                 path='/edit-profile'
                                 component={EditProfile}
-                            />{' '}
+                            />
                             <PrivateRoute
                                 exact
                                 path='/add-experience'
                                 component={AddExperience}
-                            />{' '}
+                            />
                             <PrivateRoute
                                 exact
                                 path='/add-education'
                                 component={AddEducation}
-                            />{' '}
+                            />
                         </Switch>
                     </section>
                 </Fragment>
