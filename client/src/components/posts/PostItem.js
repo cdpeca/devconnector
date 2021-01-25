790827b6daf48019c247ed42b1dce9f50417678d
cdpeca <chrispeca+github@hotmail.com>
2021-01-24 18:53:10 -0600

    updated PostItem posts component to use boolean showActions PropTypes to hide or show actions whether viewing list of posts or individual post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -11,6 +11,7 @@ const PostItem = ({
     deletePost,
     auth,
     post: { _id, text, name, avatar, user, likes, comments, date },
+    showActions,
 }) => (
     <div className='post bg-white p-1 my-1'>
         <div>
@@ -24,43 +25,51 @@ const PostItem = ({
             <p className='post-date'>
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>{' '}
             </p>
-            <Fragment>
-                <button
-                    onClick={() => addLike(_id)}
-                    type='button'
-                    className='btn btn-light'
-                >
-                    <i className='fas fa-thumbs-up' />{' '}
-                    <span>
-                        {likes.length > 0 && <span>{likes.length}</span>}
-                    </span>
-                </button>
-                <button
-                    onClick={() => removeLike(_id)}
-                    type='button'
-                    className='btn btn-light'
-                >
-                    <i className='fas fa-thumbs-down'></i>
-                </button>
-                <Link to={`/posts/${_id}`} className='btn btn-primary'>
-                    Discussion{' '}
-                    {comments.length > 0 && (
-                        <span className='comment-count'>{comments.length}</span>
-                    )}
-                </Link>
-                {!auth.loading && user === auth.user._id && (
+            {showActions && (
+                <Fragment>
                     <button
-                        onClick={() => deletePost(_id)}
+                        onClick={() => addLike(_id)}
                         type='button'
-                        className='btn btn-danger'
+                        className='btn btn-light'
                     >
-                        <i className='fas fa-times'></i>
+                        <i className='fas fa-thumbs-up' />{' '}
+                        <span>
+                            {likes.length > 0 && <span>{likes.length}</span>}
+                        </span>
                     </button>
-                )}
-            </Fragment>
+                    <button
+                        onClick={() => removeLike(_id)}
+                        type='button'
+                        className='btn btn-light'
+                    >
+                        <i className='fas fa-thumbs-down'></i>
+                    </button>
+                    <Link to={`/posts/${_id}`} className='btn btn-primary'>
+                        Discussion{' '}
+                        {comments.length > 0 && (
+                            <span className='comment-count'>
+                                {comments.length}
+                            </span>
+                        )}
+                    </Link>
+                    {!auth.loading && user === auth.user._id && (
+                        <button
+                            onClick={() => deletePost(_id)}
+                            type='button'
+                            className='btn btn-danger'
+                        >
+                            <i className='fas fa-times'></i>
+                        </button>
+                    )}
+                </Fragment>
+            )}
         </div>
     </div>
 );
+
+PostItem.defaultProps = {
+    showActions: true,
+};
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
@@ -68,6 +77,7 @@ PostItem.propTypes = {
     addLike: PropTypes.func.isRequired,
     removeLike: PropTypes.func.isRequired,
     deletePost: PropTypes.func.isRequired,
+    showActions: PropTypes.bool,
 };
 
 const mapStateToProps = state => ({
