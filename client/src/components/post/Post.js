3f2b8e73f6adabf1a28677fabddb8ddbddfdbdd0
cdpeca <chrispeca+github@hotmail.com>
2021-01-24 22:01:34 -0600

    added CommentItem post componenent into parent Post post component

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
-import CommentForm from '../post/CommentForm';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -21,6 +22,15 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
             </Link>
             <PostItem post={post} showActions={false} />
             <CommentForm postId={post._id} />
+            <div className='comments'>
+                {post.comments.map(comment => (
+                    <CommentItem
+                        key={comment._id}
+                        comment={comment}
+                        postId={post._id}
+                    />
+                ))}
+            </div>
         </Fragment>
     );
 };
