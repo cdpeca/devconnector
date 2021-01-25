2774a22e2bb20cff81dc2786fe8f119a3339b426
cdpeca <chrispeca+github@hotmail.com>
2021-01-25 01:24:07 -0600

    removed unnecessary api call

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
