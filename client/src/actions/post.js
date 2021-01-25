728e51f2a6138df8c306f40143f5879fff8a3713
cdpeca <chrispeca+github@hotmail.com>
2021-01-24 18:17:26 -0600

    created addPost function into post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import api from '../utils/api';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
+import {
+    GET_POSTS,
+    POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST,
+} from './types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -73,6 +79,28 @@ export const deletePost = id => async dispatch => {
         });
 
         dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+    try {
+        const res = await api.post('/posts', formData);
+
+        dispatch({
+            type: ADD_POST,
+            payload: res.data,
+        });
+
+        dispatch(setAlert('Post Created', 'success'));
     } catch (err) {
         dispatch({
             type: POST_ERROR,
