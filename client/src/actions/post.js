109d207c0ffef841881fe921102b39e02b4b8ce4
cdpeca <chrispeca+github@hotmail.com>
2021-01-24 19:32:53 -0600

    created addComment and removeComment functions into post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,6 +7,8 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT,
 } from './types';
 
 // Get posts
@@ -122,6 +124,50 @@ export const getPost = id => async dispatch => {
             type: GET_POST,
             payload: res.data,
         });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+    try {
+        const res = await api.post(`/posts/comment/${postId}`, formData);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data,
+        });
+
+        dispatch(setAlert('Comment Added', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+// Remove comment
+export const removeComment = (postId, commentId) => async dispatch => {
+    try {
+        await api.delete(`/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId,
+        });
+
+        dispatch(setAlert('Comment Removed', 'success'));
     } catch (err) {
         dispatch({
             type: POST_ERROR,
