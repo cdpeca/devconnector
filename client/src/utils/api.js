6312bbd925293d3084742e1736038b1de51f1a8e
cdpeca <chrispeca+github@hotmail.com>
2021-01-25 00:00:55 -0600

    updated api util to run interceptor to check for valid potentially expired token in all environments

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -17,19 +17,14 @@ const api = axios.create({
     logout the user if the token has expired
 **/
 
-// ! Only execute in production as it makes development testing incredibly difficult
-// TODO Check if this is really getting the correct response status as it seems to logout if you ever close the page
-
-if (process.env.NODE_ENV === 'production') {
-    api.interceptors.response.use(
-        res => res,
-        err => {
-            if (err.response.status === 401) {
-                store.dispatch({ type: LOGOUT });
-            }
-            return Promise.reject(err);
+api.interceptors.response.use(
+    res => res,
+    err => {
+        if (err.response.status === 401) {
+            store.dispatch({ type: LOGOUT });
         }
-    );
-}
+        return Promise.reject(err);
+    }
+);
 
 export default api;
