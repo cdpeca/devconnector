b5c84423269e40e2f8468f800c97d571c4a6e101
cdpeca <chrispeca+github@hotmail.com>
2021-01-24 19:30:21 -0600

    added ADD_COMMENT and REMOVE_COMMENT case conditions into post reducer

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -5,6 +5,8 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT,
 } from '../actions/types';
 
 const initialState = {
@@ -56,6 +58,23 @@ function postReducer(state = initialState, action) {
                         ? { ...post, likes: payload.likes }
                         : post
                 ),
+                loading: false,
+            };
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: { ...state.post, comments: payload },
+                loading: false,
+            };
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(
+                        comment => comment._id !== payload
+                    ),
+                },
                 loading: false,
             };
         default:
