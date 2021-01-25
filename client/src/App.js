eab6fbd3d3707e89c24f6e2b49233ef8205666fa
cdpeca <chrispeca+github@hotmail.com>
2021-01-24 18:54:17 -0600

    added Post post componenet into App

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -85,6 +86,11 @@ const App = () => {
                                 exact
                                 path='/posts'
                                 component={Posts}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/posts/:id'
+                                component={Post}
                             />
                         </Switch>
                     </section>
