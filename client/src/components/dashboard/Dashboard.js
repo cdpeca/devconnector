982403b36febc3f39a58767367255bf2da59ce6c
cdpeca <chrispeca+github@hotmail.com>
2021-01-25 18:08:53 -0600

    removed loading check since handled in PrivateRoute

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
@@ -12,15 +11,13 @@ const Dashboard = ({
     getCurrentProfile,
     deleteAccount,
     auth: { user },
-    profile: { profile, loading },
+    profile: { profile },
 }) => {
     useEffect(() => {
         getCurrentProfile();
     }, [getCurrentProfile]);
 
-    return loading && profile === null ? (
-        <Spinner />
-    ) : (
+    return (
         <Fragment>
             <h1 className='large text-primary'>Dashboard</h1>
             <p className='lead'>
@@ -62,7 +59,7 @@ Dashboard.propTypes = {
     profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     auth: state.auth,
     profile: state.profile,
 });
